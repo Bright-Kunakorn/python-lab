0f764ff455827c512dd04838bedaae371b8f3ed0
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2022-12-26 20:38:41 +0700

    Update and rename hw4.py to calculate_score.py

--- calculate_score.py
+++ calculate_score.py
@@ -1,17 +1,30 @@
 def calculate_score():
+    # Get the total number of students from user input
     n = int(input('Total students: '))
+
+    # Initialize variables to store the maximum and second maximum scores
     first = 0
     second = 0
+
+    # Initialize a variable to store the sum of all scores
     sum_ = 0
+
+    # Prompt the user to enter the scores
     print('Enter score: ')
     for i in range(n):
         score = int(input())
-        if score > first :
+
+        # Update the maximum and second maximum scores if necessary
+        if score > first:
             second = first
             first = score
         elif score > second and score != first:
             second = score
+
+        # Add the score to the sum
         sum_ += score
+
+    # Print the maximum, second maximum, and average scores
     print('---')
     print('Max score is: {:.2f}'.format(first))
     if second == 0:
@@ -21,7 +34,9 @@ def calculate_score():
     print('Average is: {:.2f}'.format(sum_/n))
 
 def main():
+    # Call the calculate_score function
     calculate_score()
 
+# Check if the script is being run directly and not imported as a module
 if __name__ =="__main__":
     main()
