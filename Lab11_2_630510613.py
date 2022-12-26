5a296a08b7c1887082645dccd7749b442333f1f5
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2022-12-26 20:27:34 +0700

    Update Lab11_2_630510613.py

--- Lab11_2_630510613.py
+++ Lab11_2_630510613.py
@@ -1,12 +1,14 @@
 def remove_row_col(list_a, row, col):
-    ans = list_a
+    # Create a copy of list_a
+    ans = list_a[:]
+    
+    # Iterate over the rows in ans
     for i in range(len(ans)):
-        del ans[i][col]
-    del ans[row]
+        # Remove the specified column from the current row
+        ans[i].pop(col)
+    
+    # Remove the specified row
+    ans.pop(row)
+    
+    # Return the modified list
     return ans
-
-def main():
-    print(remove_row_col([[2, 3, 4], [8, 7, 6], [0, 1, 2]], 1, 2))
-    print(remove_row_col([[2, 3, 4, 5], [8, 7, 6, 5], [0, 1, 2, 3]], 1, -3))
-if __name__=="__main__":
-    main()
