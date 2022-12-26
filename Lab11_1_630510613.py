193565fbc936bd15731d8f11d34957a18f8be61d
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2022-12-26 20:28:59 +0700

    Update Lab11_1_630510613.py

--- Lab11_1_630510613.py
+++ Lab11_1_630510613.py
@@ -1,23 +1,22 @@
-def matrix_mult(m1,m2):
-    result = [] 
+def matrix_mult(m1, m2):
+    # Initialize an empty list to store the result
+    result = []
 
+    # Iterate over the rows in m1
     for i in range(len(m1)):
-
-        row = [] 
+        # Initialize an empty list to store the result row
+        row = []
+        # Iterate over the columns in m2
         for j in range(len(m2[0])):
-
-            product = 0 
-
+            # Initialize the product to 0
+            product = 0
+            # Iterate over the elements in the row of m1 and the column of m2
             for v in range(len(m1[i])):
-                 product += m1[i][v] * m2[v][j]
-            row.append(product) 
-
-        result.append(row) 
+                # Multiply the corresponding elements and add the result to the product
+                product += m1[i][v] * m2[v][j]
+            # Append the product to the result row
+            row.append(product)
+        # Append the result row to the result
+        result.append(row)
+    # Return the result
     return result
-
-    
-def main():
-    print(matrix_mult([[1, 2, 3]], [[7, 8],[9, 10],[11,12]]))
-
-if __name__ =="__main__":
-    main()
