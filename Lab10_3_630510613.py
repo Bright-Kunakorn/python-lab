b6e18222c79e09f23c5a5a262596249971414038
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-02-17 22:59:03 +0700

    Update Lab10_3_630510613.py

--- Lab10_3_630510613.py
+++ Lab10_3_630510613.py
@@ -4,7 +4,6 @@
 #Lab10_3
 def nondest_rotate_list(list_a, n):
     return -n % len(list_a)
-
 def main():
     print(nondest_rotate_list([1,2,3,4], 1)) 
     print(nondest_rotate_list([1,2,3,4], 10))
